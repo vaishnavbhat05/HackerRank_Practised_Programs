8ac9860c58e4487cb72459a686c231ba9c0d5db4
Vaishnav <104012712+vaishnavbhat05@users.noreply.github.com>
2023-11-07 18:56:46 +0530

    Create HR_If_Else.java

---
+++ HR_If_Else.java
@@ -0,0 +1,48 @@
+// Input Format
+//A single line containing a positive integer, .
+
+// Constraints
+// Output Format
+// Print Weird if the number is weird; otherwise, print Not Weird.
+
+// Sample Input 0
+// 3
+
+// Sample Output 0
+// Weird
+
+// Sample Input 1
+// 24
+
+// Sample Output 1
+// Not Weird
+
+// Explanation
+// Sample Case 0: 
+//  is odd and odd numbers are weird, so we print Weird.
+
+// Sample Case 1: 
+//  and  is even, so it isn't weird. Thus, we print Not Weird.
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+    }
+}
