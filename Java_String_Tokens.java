09edadeaf0453d0a81b21287490154938bee559e
Vaishnav <104012712+vaishnavbhat05@users.noreply.github.com>
2023-11-23 23:59:28 +0530

    Create Java_String_Tokens.java

---
+++ Java_String_Tokens.java
@@ -0,0 +1,51 @@
+// Input Format
+
+// A single string, .
+
+// Constraints
+
+//  is composed of any of the following: English alphabetic letters, blank spaces, exclamation points (!), commas (,), question marks (?), periods (.), underscores (_), apostrophes ('), and at symbols (@).
+// Output Format
+
+// On the first line, print an integer, , denoting the number of tokens in string  (they do not need to be unique). Next, print each of the  tokens on a new line in the same order as they appear in input string .
+
+// Sample Input
+
+// He is a very very good boy, isn't he?
+// Sample Output
+
+// 10
+// He
+// is
+// a
+// very
+// very
+// good
+// boy
+// isn
+// t
+// he
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        // Tokenize the input string using regex to consider various delimiters
+        String[] tokens = s.trim().split("[ ,!,?._'@]+");
+
+        // Print the number of tokens
+        System.out.println(tokens.length);
+
+        // Print each token on a new line
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+
+        scan.close();
+    }
+}
